f49a9961da52c5234586c60b9ecfc921057e5c4b
Mattes Witthöft <matteswitti@gmail.com>
2023-08-11 01:02:20 +0200

    Initial commit from Create Next App with MUI

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { Container, Typography } from '@mui/material';
+
+export default function App() {
+  return (
+    <Container>
+      <Typography>Hello There</Typography>
+    </Container>
+  );
+}
