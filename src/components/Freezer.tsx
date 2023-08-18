2bcccc348f55aef5d0e30d146b779c183fae4e83
Mattes Witthöft <matteswitti@gmail.com>
2023-08-18 17:56:19 +0200

    expandable content for a freezer

--- src/components/Freezer.tsx
+++ src/components/Freezer.tsx
@@ -1,21 +1,57 @@
-import React from 'react';
-import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
+'use client';
 
+import React, { useState } from 'react';
+import { Box, Card, CardContent, Collapse, IconButton, IconButtonProps, Typography, styled } from '@mui/material';
+import KitchenIcon from '@mui/icons-material/Kitchen';
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 export default function Home({ freezer }: { freezer: string }) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <>
-      <Card sx={{ m: 3, display: 'flex' }} elevation={7}>
-        <Box sx={{ p: 2, display: 'flex', width: '100%' }}>
+      <Card sx={{ m: 1, mt: 3, maxWidth: '100%' }} elevation={7}>
+        <Box sx={{ p: 2, display: 'flex' }}>
           <Box>
-            <Typography variant="h5">Freezer Icon</Typography>
+            <KitchenIcon sx={{ pt: 0.7, mr: 1, fontSize: 28 }} />
           </Box>
-          <Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Typography variant="h5">{freezer}</Typography>
           </Box>
-          <Box sx={{ ml: 'auto' }}>
-            <Typography variant="h5">Collapse Icon</Typography>
+          <Box sx={{ ml: 'auto', display: 'flex', alignItems: 'center' }}>
+            {/* <ExpandMoreRoundedIcon fontSize="large" /> */}
+            <ExpandMore
+              sx={{ p: 0 }}
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more">
+              <ExpandMoreRoundedIcon sx={{ fontSize: 30 }} />
+            </ExpandMore>
           </Box>
         </Box>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography>Hello there</Typography>
+          </CardContent>
+        </Collapse>
       </Card>
     </>
   );
