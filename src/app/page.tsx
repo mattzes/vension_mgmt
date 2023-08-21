15287f737e9b92f73d6aefd0e134509f2307a7f5
Mattes Witthöft <matteswitti@gmail.com>
2023-08-21 16:14:03 +0200

    something useless

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Typography } from '@mui/material';
 
 export default function App() {
-  return <Typography>Content Here</Typography>;
+  return <Typography>Great, you found something useless :D</Typography>;
 }
