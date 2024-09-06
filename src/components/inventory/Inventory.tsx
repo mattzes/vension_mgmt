351332334ca589f990ef06aaafa5c37d9b48214d
Mattes Witthöft <matteswitti@gmail.com>
2024-09-06 22:13:49 +0200

    conditional rendering if no freezers available

--- src/components/inventory/Inventory.tsx
+++ src/components/inventory/Inventory.tsx
@@ -1,15 +1,21 @@
 import React, { useContext } from 'react';
 import { FreezerContext } from '../../context/FreezerContext';
 import InventoryCard from './InventoryCard';
+import { Typography } from '@mui/material';
+import { Freezer } from '@/general_types';
 
 export default function Inventory() {
   const { freezers } = useContext(FreezerContext);
 
   return (
     <>
-      {freezers.map(freezer => (
-        <InventoryCard key={freezer.id} freezer={freezer} />
-      ))}
+      {freezers.length === 0 ? (
+        <Typography variant="h6" align="center" mt={5}>
+          Derzeit sind keine Gefriertruhen angelegt.
+        </Typography>
+      ) : (
+        freezers.map(freezer => <InventoryCard key={freezer.id} freezer={freezer} />)
+      )}
     </>
   );
 }
