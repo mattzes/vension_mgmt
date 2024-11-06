b1f42851cb1afbae2d99f9aa9fbb83077848fe5d
Mattes Witthöft <matteswitti@gmail.com>
2024-11-06 16:13:24 +0100

    feat: implement get all prices as api route

--- src/app/api/price/route.tsx
+++ src/app/api/price/route.tsx
@@ -1,8 +1,29 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { prices } from '@/mocked_general_data';
+import { collection, doc, getDocs, addDoc, updateDoc, deleteDoc } from 'firebase/firestore';
+import { db } from '@/util/firebaseConfig';
 
 export async function GET() {
-  return NextResponse.json(prices);
+  try {
+    const querySnapshot = await getDocs(collection(db, 'animalParts'));
+    const animalParts = querySnapshot.docs.map(doc => ({
+      ...doc.data(),
+    }));
+
+    const prices = [];
+    for (const animalPart of animalParts) {
+      for (const [key, value] of Object.entries(animalPart.parts)) {
+        prices.push({
+          animal: animalPart.name,
+          animalPart: key,
+          price: value,
+        });
+      }
+    }
+
+    return NextResponse.json(prices);
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
 
 export async function POST(req: NextRequest) {
