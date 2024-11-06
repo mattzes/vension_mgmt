8a173a9f23dc4f886bcc22b656fffa5a14043f7d
Mattes Witthöft <matteswitti@gmail.com>
2024-11-06 01:00:55 +0100

    feat: implement alle freezer routes and its database logic except validation

--- src/app/api/freezer/route.tsx
+++ src/app/api/freezer/route.tsx
@@ -1,16 +1,67 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { freezers_only } from '@/mocked_general_data';
+import { db } from '@/util/firebaseConfig';
+import { collection, doc, getDoc, getDocs, addDoc, updateDoc, deleteDoc } from 'firebase/firestore';
 
 export async function GET() {
-  return NextResponse.json(freezers_only);
+  try {
+    const querySnapshot = await getDocs(collection(db, 'freezer'));
+    const freezers = querySnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    return NextResponse.json(freezers);
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
 
 export async function POST(req: NextRequest) {
-  console.log(await req.json());
-  return NextResponse.json({ message: 'success' }, { status: 201 });
+  try {
+    const data = await req.json();
+
+    // validate data
+
+    const docRef = await addDoc(collection(db, 'freezer'), data);
+
+    return NextResponse.json({ id: docRef.id, message: 'success' }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
 
 export async function PUT(req: NextRequest) {
-  console.log(await req.json());
-  return NextResponse.json({ message: 'success' }, { status: 202 });
+  try {
+    const data = await req.json();
+
+    // validate data
+
+    const docRef = doc(db, 'freezer', data.id);
+
+    await updateDoc(docRef, data);
+
+    return NextResponse.json({ message: 'success' }, { status: 202 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const data = await req.json();
+
+    const docRef = doc(db, 'freezer', data.id);
+
+    const docSnap = await getDoc(docRef);
+    if (!docSnap.exists()) {
+      // Return a message if the document doesn't exist
+      return NextResponse.json({ error: 'No document found to delete' }, { status: 404 });
+    }
+
+    await deleteDoc(docRef);
+
+    return NextResponse.json({ message: 'success' }, { status: 202 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
