cb6ff8936709be621cd3a5b91d83edd698225349
Mattes Witthöft <matteswitti@gmail.com>
2024-11-06 16:56:03 +0100

    feat: implement update and delete api routes for prices

--- src/app/api/price/route.tsx
+++ src/app/api/price/route.tsx
@@ -40,6 +40,10 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: 'Multiple documents found' }, { status: 500 });
     } else {
       const docRef = animalPartsSnapshot.docs[0].ref;
+      const docData = animalPartsSnapshot.docs[0].data();
+      if (docData.parts[animalPart]) {
+        return NextResponse.json({ message: 'Animal part already exists' }, { status: 409 });
+      }
       await updateDoc(docRef, {
         [`parts.${animalPart}`]: price, // Use Firestore's dot notation for nested fields
       });
@@ -52,6 +56,69 @@ export async function POST(req: NextRequest) {
 }
 
 export async function PUT(req: NextRequest) {
-  console.log(await req.json());
-  return NextResponse.json({ message: 'success' }, { status: 202 });
+  try {
+    const { animal, animalPart, price } = await req.json();
+    const animalPartsRef = collection(db, 'animalParts');
+    const animalPartsQuery = query(animalPartsRef, where('name', '==', animal));
+    const animalPartsSnapshot = await getDocs(animalPartsQuery);
+
+    if (animalPartsSnapshot.empty) {
+      return NextResponse.json({ message: 'Animal does not exist' }, { status: 409 });
+    } else if (animalPartsSnapshot.docs.length > 1) {
+      return NextResponse.json({ message: 'Multiple documents found' }, { status: 500 });
+    } else {
+      const docRef = animalPartsSnapshot.docs[0].ref;
+      const docData = animalPartsSnapshot.docs[0].data();
+      if (!docData.parts[animalPart]) {
+        return NextResponse.json({ message: 'Animal part does not exist' }, { status: 409 });
+      }
+      await updateDoc(docRef, {
+        [`parts.${animalPart}`]: price, // Use Firestore's dot notation for nested fields
+      });
+    }
+
+    return NextResponse.json({ message: 'success' }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const { animal, animalPart, price } = await req.json();
+    const animalPartsRef = collection(db, 'animalParts');
+    const animalPartsQuery = query(animalPartsRef, where('name', '==', animal));
+    const animalPartsSnapshot = await getDocs(animalPartsQuery);
+
+    if (animalPartsSnapshot.empty) {
+      return NextResponse.json({ message: 'Document not found' }, { status: 404 });
+    } else if (animalPartsSnapshot.docs.length > 1) {
+      return NextResponse.json({ message: 'Multiple documents found' }, { status: 500 });
+    } else {
+      const animalPartData = animalPartsSnapshot.docs[0].data();
+      const toUpdateAnimalPart = animalPartsSnapshot.docs[0].ref;
+
+      if (!animalPartData.parts[animalPart]) {
+        return NextResponse.json({ message: 'Animal part not found' }, { status: 404 });
+      }
+
+      if (Object.keys(animalPartData.parts).length === 1) {
+        await deleteDoc(toUpdateAnimalPart);
+        return NextResponse.json({ message: 'success' }, { status: 202 });
+      }
+
+      if (Object.keys(animalPartData.parts).length > 1) {
+        const newParts = { ...animalPartData.parts };
+        delete newParts[animalPart];
+        await updateDoc(toUpdateAnimalPart, {
+          parts: newParts,
+        });
+      }
+    }
+
+    return NextResponse.json({ message: 'success' });
+    1;
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
