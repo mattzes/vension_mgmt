d609e259bb7539aef1ba4bd0bf22674dd265fa55
Mattes Witthöft <matteswitti@gmail.com>
2024-11-06 16:00:50 +0100

    feat: implement update route for a single item

--- src/app/api/item/route.tsx
+++ src/app/api/item/route.tsx
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { collection, Timestamp, addDoc } from 'firebase/firestore';
+import { collection, Timestamp, doc, getDoc, addDoc, updateDoc } from 'firebase/firestore';
 import { db } from '@/util/firebaseConfig';
 
 export async function POST(req: NextRequest) {
@@ -22,8 +22,27 @@ export async function POST(req: NextRequest) {
 }
 
 export async function PUT(req: NextRequest) {
-  console.log(await req.json());
-  return NextResponse.json({ message: 'success' }, { status: 202 });
+  try {
+    const data = await req.json();
+
+    // validate data
+
+    const { id, ...dataToUpdate } = data;
+
+    const docRef = doc(db, 'item', id);
+
+    const docSnap = await getDoc(docRef);
+    if (!docSnap.exists()) {
+      // Return a message if the document doesn't exist
+      return NextResponse.json({ error: `No document found to update with id: ${id}` }, { status: 404 });
+    }
+
+    await updateDoc(docRef, dataToUpdate);
+
+    return NextResponse.json({ message: 'success' }, { status: 202 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
 
 export async function DELETE(req: NextRequest) {
