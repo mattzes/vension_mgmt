95feae2ea090543f0b278517ced832d6a61b30c7
Mattes Witthöft <matteswitti@gmail.com>
2024-11-06 15:43:56 +0100

    feat: implement add vension api route

--- src/app/api/item/route.tsx
+++ src/app/api/item/route.tsx
@@ -1,9 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { freezers_only } from '@/mocked_general_data';
+import { collection, Timestamp, addDoc } from 'firebase/firestore';
+import { db } from '@/util/firebaseConfig';
 
 export async function POST(req: NextRequest) {
-  console.log(await req.json());
-  return NextResponse.json({ message: 'success' }, { status: 201 });
+  try {
+    const data = await req.json();
+
+    // validate data
+
+    const dataToStore = {
+      ...data,
+      date: Timestamp.fromDate(new Date(data.date)),
+    };
+
+    const docRef = await addDoc(collection(db, 'item'), dataToStore);
+
+    return NextResponse.json({ id: docRef.id, message: 'success' }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
 
 export async function PUT(req: NextRequest) {
