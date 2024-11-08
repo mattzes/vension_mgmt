92bc7aa41a8fc8067eb8e6b3a4e62da3f5810370
Mattes Witthöft <matteswitti@gmail.com>
2024-11-08 01:34:34 +0100

    feat: change relativ position of alert

--- src/components/alerts/Alert.tsx
+++ src/components/alerts/Alert.tsx
@@ -1,13 +1,15 @@
 import { useContext } from 'react';
-import { Alert as MuiAlert } from '@mui/material';
+import { Alert as MuiAlert, Snackbar } from '@mui/material';
 import { AlertContext } from '@/context/AlertContext';
 
 export function Alert() {
   const { alertHandleClose, alertData } = useContext(AlertContext);
 
   return (
-    <MuiAlert onClose={alertHandleClose} severity={alertData.type}>
-      {alertData.message}
-    </MuiAlert>
+    <Snackbar open={true} anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
+      <MuiAlert onClose={alertHandleClose} severity={alertData.type} variant="filled">
+        {alertData.message}
+      </MuiAlert>
+    </Snackbar>
   );
 }
