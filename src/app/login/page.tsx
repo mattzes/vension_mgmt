d7b3515d0bd876e1a34030d4ca32a85684badc79
Mattes Witthöft <matteswitti@gmail.com>
2024-11-15 00:36:19 +0100

    fix: typo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     <Container disableGutters>
       <Box display="flex" justifyContent="center" alignItems="center" mt={5}>
         <Typography variant="h6" align="center" mt={5}>
-          Bitt melde dich an, um die Seite zu nutzen.
+          Bitte melde dich an, um die Seite zu nutzen.
         </Typography>
       </Box>
     </Container>
