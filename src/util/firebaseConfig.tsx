cc29aa3b31f78578830387aa81109efe287e8ac8
Mattes Witthöft <matteswitti@gmail.com>
2024-11-14 15:53:43 +0100

    refactor: remove frontend db access

--- src/util/firebaseConfig.tsx
+++ src/util/firebaseConfig.tsx
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -17,5 +16,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const auth = getAuth(app);
