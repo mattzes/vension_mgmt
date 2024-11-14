4df0f7c45533d069a8faadfeec7dd6daf79c1758
Mattes Witthöft <matteswitti@gmail.com>
2024-11-14 03:20:11 +0100

    feat: implement user specific items and firebase admin within item route

--- src/app/api/item/route.tsx
+++ src/app/api/item/route.tsx
@@ -1,22 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { collection, Timestamp, doc, getDoc, addDoc, updateDoc, deleteDoc } from 'firebase/firestore';
-import { db } from '@/util/firebaseConfig';
+import { db } from '@/util/firebaseAdmin';
+import { Timestamp } from 'firebase-admin/firestore';
 import { getPrice } from '@/app/api/price/route';
 
 export async function POST(req: NextRequest) {
   try {
     const data = await req.json();
+    const userId = req.headers.get('x-user-uid');
 
-    // validate data
+    // Validate data here
 
     const dataToStore = {
       ...data,
+      userId,
       date: Timestamp.fromDate(new Date(data.date)),
     };
 
-    const docRef = await addDoc(collection(db, 'item'), dataToStore);
-    const docSnap = await getDoc(docRef);
-    const createdData = docSnap.data();
+    const docRef = await db.collection('item').add(dataToStore);
+    const createdData = (await docRef.get()).data();
+
+    if (!createdData) {
+      return NextResponse.json({ message: 'Fehler beim erstellen des Gegenstandes.' }, { status: 500 });
+    }
 
     const animalPrice = await getPrice(createdData.animal, createdData.animalPart);
 
@@ -24,36 +29,37 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: animalPrice.message }, { status: animalPrice.status });
     }
 
+    delete createdData.userId;
     const item = {
-      id: docSnap.id,
+      id: docRef.id,
       ...createdData,
-      date: createdData.date.seconds * 1000 + createdData.date.nanoseconds / 1000000,
+      date: createdData.date.toMillis(),
       price: animalPrice.price,
     };
 
     return NextResponse.json({ item: item, message: 'success' }, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ message: error }, { status: 500 });
+    return NextResponse.json({ message: error || 'Internal Server Error' }, { status: 500 });
   }
 }
 
 export async function PUT(req: NextRequest) {
   try {
     const data = await req.json();
+    const userId = req.headers.get('x-user-uid');
 
     // validate data
 
     const { id, ...dataToUpdate } = data;
 
-    const docRef = doc(db, 'item', id);
+    const docRef = db.collection('item').doc(id);
+    const docSnap = await docRef.get();
 
-    const docSnap = await getDoc(docRef);
-    if (!docSnap.exists()) {
-      // Return a message if the document doesn't exist
-      return NextResponse.json({ message: 'Es wurde kein Dokument zu den Daten gefunden' }, { status: 404 });
+    if (!docSnap.exists || docSnap.data()?.userId !== userId) {
+      return NextResponse.json({ message: 'Es wurde kein Dokument zu den Daten gefunden.' }, { status: 404 });
     }
 
-    await updateDoc(docRef, dataToUpdate);
+    await docRef.update(dataToUpdate);
 
     return NextResponse.json({ message: 'success' }, { status: 202 });
   } catch (error) {
@@ -64,16 +70,16 @@ export async function PUT(req: NextRequest) {
 export async function DELETE(req: NextRequest) {
   try {
     const { id } = await req.json();
+    const userId = req.headers.get('x-user-uid');
 
-    const docRef = doc(db, 'item', id);
+    const docRef = db.collection('item').doc(id);
+    const docSnap = await docRef.get();
 
-    const docSnap = await getDoc(docRef);
-    if (!docSnap.exists()) {
-      // Return a message if the document doesn't exist
+    if (!docSnap.exists || docSnap.data()?.userId !== userId) {
       return NextResponse.json({ message: 'Es wurde kein Dokument zu den Daten gefunden' }, { status: 404 });
     }
 
-    await deleteDoc(docRef);
+    await docRef.delete();
 
     return NextResponse.json({ message: 'success' }, { status: 202 });
   } catch (error) {
